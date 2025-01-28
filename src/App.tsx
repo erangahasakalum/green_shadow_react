73c2f0d68f81946ff184aee3104af4b986805686
Eranga Hasakalum <erangahasakalum6761@gmail.com>
2025-01-28 22:12:26 +0530

    install react dom and create field page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import RootLayout from "./components/RootLayout";
+import { DashBoard } from "./pages/DashBoard";
+import { CropPage } from "./pages/crop/CropPage";
 function App() {
+
+  const routes = createBrowserRouter([
+    {
+      path:'',
+      element:<RootLayout/>,
+      children:[
+        { path: '', element: <DashBoard /> },
+        {path:'/crop-manage',element:<CropPage/>}
+        
+        
+      ]
+    }
+  ])
   return (
     <>
-      <RootLayout/>
+      <RouterProvider router={routes}/>
     </>
   );
 }
