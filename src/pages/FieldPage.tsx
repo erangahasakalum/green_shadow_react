d6c8ffe61fc1e7dc95b726bcfa4cf91c1b96355f
Eranga Hasakalum <erangahasakalum6761@gmail.com>
2025-02-14 00:20:49 +0530

    add titles

--- src/pages/FieldPage.tsx
+++ src/pages/FieldPage.tsx
@@ -4,29 +4,29 @@ import { useState } from "react";
 import FieldForm from "../components/forms/FieldForm";
 import MainModal from "../components/modal/MainModal";
 
+export function FieldPage() {
+  const [open, setOpen] = useState(false);
+  const [title, setTitle] = useState("");
 
-export function FieldPage(){
+  function openModal() {
+    setTitle("Add New Field");
+    setOpen(true);
+  }
+  return (
+    <>
+      <div>
+        <h1>field</h1>
+        <Button type="primary" onClick={openModal}>
+          Add Field
+        </Button>
+        <MainModal open={open} setOpen={setOpen} title={title}>
+          <FieldForm />
+        </MainModal>
+      </div>
 
-    const [open,setOpen] = useState(false);
-    const [title,setTitle] = useState("");
-
-    function openModal(){
-        setTitle("Add New Field");
-        setOpen(true)
-    }
-    return(
-        <>
-            <div>
-                <h1>field</h1>
-                <Button type="primary" onClick={openModal}>Add Field</Button> 
-                <MainModal open={open} setOpen={setOpen} title={title}>
-                   <FieldForm />
-                 </MainModal>    
-            </div>
-
-            <div>
-                <TableComponents/>
-            </div>
-        </>
-    )
+      <div>
+        <TableComponents />
+      </div>
+    </>
+  );
 }
--- src/pages/VehiclePage.tsx
+++ src/pages/VehiclePage.tsx
@@ -1,27 +1,28 @@
-import { Button } from "antd"
-import { useState } from "react"
+import { Button } from "antd";
+import { useState } from "react";
 import MainModal from "../components/modal/MainModal";
 import VehicleForm from "../components/forms/VehicleForm";
 import TableComponents from "../components/table/TableComponent";
 
-export function VehiclePage(){
-    
-    const [open,setOpen] = useState(false);
-    function addVehicle(){
-            setOpen(true);
-    }
-    return(
-        <>
-        <div>
+export function VehiclePage() {
+  const [open, setOpen] = useState(false);
+  const [title, setTitle] = useState("");
+  function addVehicle() {
+    setTitle("Add New Vehicle");
+    setOpen(true);
+  }
+  return (
+    <>
+      <div>
         <h1>Vehicle</h1>
-        <Button type="primary"onClick={()=>addVehicle()}>Add Vehicle</Button>
-        <MainModal open={open} setOpen={setOpen}>
-         <VehicleForm/>
+        <Button type="primary" onClick={() => addVehicle()}>
+          Add Vehicle
+        </Button>
+        <MainModal open={open} setOpen={setOpen} title={title}>
+          <VehicleForm />
         </MainModal>
-        </div>
-        <div>
-            <TableComponents/>
-        </div>
-        </>
-    )
+      </div>
+      <TableComponents />
+    </>
+  );
 }
